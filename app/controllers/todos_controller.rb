f9e4cfc57f01f5d9d2e580588cb0d7b01f432673
Michael <Michael@Sprauer.net>
2015-03-19 00:16:39 +0100

    create todos RESTfully

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -2,4 +2,15 @@ class TodosController < ApplicationController
   def index
     render json: Todo.all
   end
+
+  def create
+    todo = Todo.create! resource_params
+    render json: todo, status: :created
+  end
+
+  private
+    def resource_params
+      params.require(:todo).permit(:id, :title, :is_completed)
+    end
+
 end
